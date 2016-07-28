65939b9295d91584b06b888b9377710e71978235
Alberto De Bortoli <alberto.debortoli@just-eat.com>
2016-07-28 20:46:54 +0300

    Initial Commit

---
+++ Skopelos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Skopelos
+//
+//  Created by Alberto DeBortoli on 28/07/2016.
+//  Copyright © 2016 Alberto De Bortoli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
